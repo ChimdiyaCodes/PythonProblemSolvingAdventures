d8cf7f38bc67fa39add72c42679acb90e540bca4
DiyaCodes <chimdiyaconstance@gmail.com>
2025-02-12 00:23:38 +0100

    Write a program that asks the user to enter his or her name. The program should respond with a message that says hello to the user, using his or her name.

---
+++ exercise_2.py
@@ -0,0 +1,7 @@
+# Exercise 2:
+# Write a program that asks the user to enter their name.
+# The program should respond with a message that says hello to the user.
+
+# Solution:
+name = input("What is your name? ").strip().title()
+print(f"Hello, {name}! Nice to meet you.")
