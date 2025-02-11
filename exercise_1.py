b7d2ed68dad93ca56f32edd56d9cdb3a6106d79b
DiyaCodes <chimdiyaconstance@gmail.com>
2025-02-12 00:06:27 +0100

    Create a program that displays your name and complete mailing address formatted in the manner that you would usually see it on the outside of an envelope. Your program does not need to read any input from the user.

---
+++ exercise_1.py
@@ -0,0 +1,14 @@
+# EXERCISE 1:
+
+# Create a program that displays your name and complete mailing address formatted in
+# the manner that you would usually see it on the outside of an envelope. Your program
+# does not need to read any input from the user.
+
+
+# SOLUTION:
+
+print("""\
+Constance Chimdiya Ihekerem
+Harmony Road
+Lagos, Nigeria
+""")
